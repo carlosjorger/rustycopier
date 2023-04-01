9ae3a80d4c2b79add42cb826d19f70673e585433
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-04-01 00:56:30 -0400

    define the two parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len()<=1 {
+      println!("Please pass the origin path");
+      return;
+    }
+    if args.len()<=2 {
+      println!("Please pass the destiny path");
+      return;
+    }
+    let file_path=&args[1];
+        let contents = fs::read(file_path)
+          .expect("Should have been able to read the file");
+
+    for data in contents {
+      println!("{data}");
+    }
 }
