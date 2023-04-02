56025465bf31b46c5fab527498114cb698677028
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-04-01 22:18:45 -0400

    create BufferReader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,53 @@
-use std::fs;
-use std::env;
+
+use std::{env, time::Instant};
+
+mod buffer_reader{
+  use std::fs::File;
+  use std::io::{BufReader, BufRead};
+ pub struct BufferReader{
+      curr: u128,
+      next: u128,
+      reader: BufReader<File>,
+      capacity:usize
+  }
+  
+  impl Iterator for BufferReader{
+
+      type Item= u128;
+      fn next(&mut self) -> Option<Self::Item> {
+        
+        let buffer=self.reader.fill_buf().expect("error in the buffer");
+        let lenght= buffer.len();
+        if lenght==self.capacity {
+            self.curr=self.next;
+            self.next=u128::from_be_bytes(buffer.try_into().expect("incorrect length"));
+            self.reader.consume(lenght);
+            Some(self.curr)
+        }
+        else if lenght<self.capacity&& lenght>0 {
+            self.curr=self.next;
+            let mut complete_buffer:Vec<u8>=vec![0;self.capacity-lenght];
+            let mut incomplete_buffer= buffer.to_vec();
+            complete_buffer.append(&mut incomplete_buffer);
+            self.next=u128::from_be_bytes(complete_buffer.try_into().expect("incorrect length"));
+            self.reader.consume(lenght);
+            Some(self.curr)
+        }
+        else{
+           None
+        }
+        
+      }
+  }
+  pub fn get_buffer(source_path:&str)->BufferReader{
+    let file = File::open(source_path)
+          .expect("Should have been able to read the file");
+    let reader=BufReader::with_capacity(16, file);
+    BufferReader { curr: 0, next: 0, reader: reader, capacity: 16 }
+  }
+}
+
+use crate::buffer_reader::get_buffer;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,11 +59,13 @@ fn main() {
       println!("Please pass the destiny path");
       return;
     }
-    let file_path=&args[1];
-        let contents = fs::read(file_path)
-          .expect("Should have been able to read the file");
-
-    for data in contents {
-      println!("{data}");
+    let source_path=&args[1];
+    let buffer_reader=get_buffer(source_path);
+    let start = Instant::now();
+    for buffer in buffer_reader {
+        //  println!("> {:?}", buffer);
     }
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
+
