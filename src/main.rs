be43f3fb16a6ac1d9f7c7839dbe71fcc69c30182
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-04-06 22:58:55 -0400

    format the code

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,67 @@
-
-
 mod progress_bar;
 
 use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 use std::{env, time::Instant};
-use std::io::{BufWriter, Write, BufReader, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len()<=1 {
-      println!("Please pass the origin path");
-      return;
+    if args.len() <= 1 {
+        println!("Please pass the origin path");
+        return;
     }
-    if args.len()<=2 {
-      println!("Please pass the destiny path");
-      return;
+    if args.len() <= 2 {
+        println!("Please pass the destiny path");
+        return;
     }
- 
-    let source=&args[1];
-    let destiny=&args[2];
+
+    let source = &args[1];
+    let destiny = &args[2];
     let file_name = &get_file_name_from_path(source);
-    let destiny_with_file=add_file_name_to_path(destiny, file_name);
+    let destiny_with_file = add_file_name_to_path(destiny, file_name);
     let start = Instant::now();
-    copy(&source, &destiny_with_file,1024*1000);
+    copy(&source, &destiny_with_file, 1024 * 1000);
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
-fn get_file_name_from_path(path:&str)->String{
-  let path= Path::new(&path);
-  let file_name = path.file_name()
-                    .expect("the origin path is not a valid file")
-                    .to_str().unwrap();
-  file_name.to_string()
+fn get_file_name_from_path(path: &str) -> String {
+    let path = Path::new(&path);
+    let file_name = path
+        .file_name()
+        .expect("the origin path is not a valid file")
+        .to_str()
+        .unwrap();
+    file_name.to_string()
 }
-fn add_file_name_to_path(path:&str,file_name:&str)->String{
-  let path=Path::new(&path);
-  let path_with_file_name=path.join(file_name);
-  let path_str=path_with_file_name.to_str().unwrap();
-  path_str.to_string()
+fn add_file_name_to_path(path: &str, file_name: &str) -> String {
+    let path = Path::new(&path);
+    let path_with_file_name = path.join(file_name);
+    let path_str = path_with_file_name.to_str().unwrap();
+    path_str.to_string()
 }
-fn copy(source_path:&str,destiny_path:&str,capacity:usize) {
-  let destiny_file = File::create(destiny_path)
-  .expect("Should have been able to read the destiny path");
-  let source_file = File::open(source_path)
-  .expect("Should have been able to read the source path");
+fn copy(source_path: &str, destiny_path: &str, capacity: usize) {
+    let destiny_file =
+        File::create(destiny_path).expect("Should have been able to read the destiny path");
+    let source_file =
+        File::open(source_path).expect("Should have been able to read the source path");
 
-  let mut stream =BufWriter::with_capacity(capacity, &destiny_file);
-  let mut reader=BufReader::with_capacity(capacity, &source_file);
+    let mut stream = BufWriter::with_capacity(capacity, &destiny_file);
+    let mut reader = BufReader::with_capacity(capacity, &source_file);
 
-  let size= source_file.metadata().unwrap().len() as usize;
-  let mut progress=progress_bar::ProgressBar::from_total_size(size);
-  println!("");
-  loop {
-      let buffer=reader.fill_buf().expect("error in the buffer");
-      let lenght= buffer.len();
-      stream.write(buffer).expect("error to write");
-      reader.consume(lenght);
-      progress.consume(lenght);
-      if lenght==0 {
-          break;
-      }
-  }
-  println!()
-
+    let size = source_file.metadata().unwrap().len() as usize;
+    let mut progress = progress_bar::ProgressBar::from_total_size(size);
+    println!("");
+    loop {
+        let buffer = reader.fill_buf().expect("error in the buffer");
+        let lenght = buffer.len();
+        stream.write(buffer).expect("error to write");
+        reader.consume(lenght);
+        progress.consume(lenght);
+        if lenght == 0 {
+            break;
+        }
+    }
+    println!()
 }
-
