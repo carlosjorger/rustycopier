2cac44ae69b0e7d884e71ac2824a4d84fa2d4733
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-04-29 15:54:32 -0400

    fix progress bar bug

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -1,5 +1,8 @@
 use crossterm::{cursor, terminal, QueueableCommand};
-use std::io::{stdout, Stdout, Write};
+use std::{
+    cmp::min,
+    io::{stdout, Stdout, Write},
+};
 
 struct ProgressBarDrawer {
     stdout: Stdout,
@@ -45,7 +48,7 @@ pub struct ProgressBar {
     consumed_size: usize,
     approximate_progres: usize,
     progress_bar: ProgressBarDrawer,
-    number_of_bars: usize,
+    total_of_bars: usize,
 }
 impl ProgressBar {
     pub fn from_total_size(total_size: usize) -> Self {
@@ -55,23 +58,22 @@ impl ProgressBar {
             consumed_size: 0,
             approximate_progres: 0,
             progress_bar: ProgressBarDrawer::progress_bar(NUMBER_OF_BARS),
-            number_of_bars: NUMBER_OF_BARS,
+            total_of_bars: NUMBER_OF_BARS,
         }
     }
-    fn percent_of_consume(&self) -> f64 {
-        (self.consumed_size as f64) / (self.total_size as f64)
-    }
-    fn percent_of_progress(&self) -> f64 {
-        (self.approximate_progres as f64) / (self.number_of_bars as f64)
-    }
+
     pub fn set_new_file(&mut self, file_name: &str) {
         self.progress_bar.print_new_file(file_name);
     }
     pub fn consume(&mut self, lenght: usize) {
         self.consumed_size += lenght;
-        if self.percent_of_consume() > self.percent_of_progress() {
-            self.approximate_progres += 1;
-            self.progress_bar.draw_a_bar(self.approximate_progres);
-        }
+        self.approximate_progres = min(
+            (self.percent_of_consume() * (self.total_of_bars as f64).round()) as usize,
+            self.total_of_bars,
+        );
+        self.progress_bar.draw_a_bar(self.approximate_progres);
+    }
+    fn percent_of_consume(&self) -> f64 {
+        (self.consumed_size as f64) / (self.total_size as f64)
     }
 }
