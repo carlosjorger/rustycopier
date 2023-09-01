c85b3b5091c5af1252453249bc530602b7916e3b
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-01 11:25:41 -0400

    🦄 refactor: refactor test

--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,13 @@
+use crate::copier::FileToCopy;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use std::{
     env::temp_dir,
     fs::{self, File},
     io::{self, Write},
     path::{Path, PathBuf},
 };
-
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use tempdir::TempDir;
 
-use crate::copier::FileToCopy;
 fn create_file(file_path: &PathBuf, content: &[u8]) -> Result<(), io::Error> {
     let mut file_source = File::create(file_path)?;
 
@@ -32,25 +31,7 @@ fn copy_one_file_in_a_folder() {
 
     let file_source_path: std::path::PathBuf = source_dir.path().join("poetry.txt");
 
-    let msg = b"In the world of coding, a language stands
-                            Rust, they call it, with its own demands
-                            It's strict and crotchety, some may say
-                            But its power and speed are here to stay
-
-                            Its syntax may seem a bit obtuse
-                            But its memory safety is no excuse
-                            For sloppy code that could bring down
-                            An entire system with just one frown
-
-                            Rust is compiled, not interpreted
-                            And its performance can't be debated
-                            It's perfect for systems and low-level tasks
-                            And its community is growing fast
-
-                            So if you're feeling crotchety today
-                            Give Rust a chance, don't turn away
-                            It may be strict, but it's worth the fight
-                            For a language that's powerful and right.";
+    let msg = get_example_text();
     create_file(&file_source_path, msg).expect("unable to create the file");
 
     let source_dir_str = source_dir.path().to_str().unwrap();
@@ -73,25 +54,7 @@ fn copy_one_file() {
 
     let file_source_path: std::path::PathBuf = source_dir.join("poetry.txt");
 
-    let msg = b"In the world of coding, a language stands
-                            Rust, they call it, with its own demands
-                            It's strict and crotchety, some may say
-                            But its power and speed are here to stay
-
-                            Its syntax may seem a bit obtuse
-                            But its memory safety is no excuse
-                            For sloppy code that could bring down
-                            An entire system with just one frown
-
-                            Rust is compiled, not interpreted
-                            And its performance can't be debated
-                            It's perfect for systems and low-level tasks
-                            And its community is growing fast
-
-                            So if you're feeling crotchety today
-                            Give Rust a chance, don't turn away
-                            It may be strict, but it's worth the fight
-                            For a language that's powerful and right.";
+    let msg = get_example_text();
     create_file(&file_source_path, msg).unwrap();
 
     let source_dir_str = file_source_path.to_str().unwrap();
@@ -108,25 +71,7 @@ fn copy_one_file() {
 fn copy_10000_files() {
     const NUMBER_OF_FILES: usize = 10000;
     let source_dir = TempDir::new("my_source_dir").expect("unable create a dir");
-    let msg = b"In the world of coding, a language stands
-                            Rust, they call it, with its own demands
-                            It's strict and crotchety, some may say
-                            But its power and speed are here to stay
-
-                            Its syntax may seem a bit obtuse
-                            But its memory safety is no excuse
-                            For sloppy code that could bring down
-                            An entire system with just one frown
-
-                            Rust is compiled, not interpreted
-                            And its performance can't be debated
-                            It's perfect for systems and low-level tasks
-                            And its community is growing fast
-
-                            So if you're feeling crotchety today
-                            Give Rust a chance, don't turn away
-                            It may be strict, but it's worth the fight
-                            For a language that's powerful and right.";
+    let msg = get_example_text();
     for file_number in 0..NUMBER_OF_FILES {
         let file_source_path: std::path::PathBuf =
             source_dir.path().join(format!("poetry{}.txt", file_number));
@@ -156,25 +101,7 @@ fn copy_10000_files() {
 fn copy_5000_files() {
     const NUMBER_OF_FILES: usize = 5000;
     let source_dir = TempDir::new("my_source_dir").expect("unable create a dir");
-    let msg = b"In the world of coding, a language stands
-                            Rust, they call it, with its own demands
-                            It's strict and crotchety, some may say
-                            But its power and speed are here to stay
-
-                            Its syntax may seem a bit obtuse
-                            But its memory safety is no excuse
-                            For sloppy code that could bring down
-                            An entire system with just one frown
-
-                            Rust is compiled, not interpreted
-                            And its performance can't be debated
-                            It's perfect for systems and low-level tasks
-                            And its community is growing fast
-
-                            So if you're feeling crotchety today
-                            Give Rust a chance, don't turn away
-                            It may be strict, but it's worth the fight
-                            For a language that's powerful and right.";
+    let msg = get_example_text();
     for file_number in 0..NUMBER_OF_FILES {
         let file_source_path: std::path::PathBuf =
             source_dir.path().join(format!("poetry{}.txt", file_number));
@@ -196,4 +123,25 @@ fn copy_5000_files() {
             let new_msg = fs::read(file_destiny_path).unwrap();
             assert_eq!(msg_vector, new_msg);
         });
+}
+fn get_example_text<'a>() -> &'a [u8; 1037] {
+    b"In the world of coding, a language stands
+                            Rust, they call it, with its own demands
+                            It's strict and crotchety, some may say
+                            But its power and speed are here to stay
+
+                            Its syntax may seem a bit obtuse
+                            But its memory safety is no excuse
+                            For sloppy code that could bring down
+                            An entire system with just one frown
+
+                            Rust is compiled, not interpreted
+                            And its performance can't be debated
+                            It's perfect for systems and low-level tasks
+                            And its community is growing fast
+
+                            So if you're feeling crotchety today
+                            Give Rust a chance, don't turn away
+                            It may be strict, but it's worth the fight
+                            For a language that's powerful and right."
 }
