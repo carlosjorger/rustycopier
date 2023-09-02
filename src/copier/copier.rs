4931700499252fbe65c3836a623216c1a6552f36
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-02 08:43:54 -0400

    🦄 refactor: create separate module file_copy

--- src/copier/copier.rs
+++ src/copier/copier.rs
@@ -1,50 +1,7 @@
-use crate::{
-    copier_pool::CopierPool,
-    progress_counter::{ProgressBar, ProgressCounter},
-};
-use std::{
-    fs::File,
-    io::{self, BufRead, BufReader, BufWriter, Write},
-    path::PathBuf,
-};
-// TODO: create separete module for this type
-pub struct FileCopy {
-    source_file_path: PathBuf,
-    source_file: File,
-    target_file: File,
-}
-impl FileCopy {
-    pub fn from_files(target_file_path: PathBuf, source_file_path: PathBuf) -> Self {
-        let source_file: File = File::open(&source_file_path).expect("error opening the file");
-        let target_file = File::create(target_file_path).expect("error creating the file");
-        Self {
-            source_file_path,
-            source_file,
-            target_file,
-        }
-    }
-    fn create_file<T: ProgressCounter>(&mut self, progress_bar: &mut T) -> Result<(), io::Error> {
-        progress_bar.set_new_file(&self.source_file_path);
+use crate::{copier_pool::CopierPool, progress_counter::ProgressBar};
 
-        self.copy_file(progress_bar, 1024 * 500);
-        Ok(())
-    }
-    fn copy_file<T: ProgressCounter>(&self, progress_bar: &mut T, capacity: usize) {
-        let mut stream = BufWriter::with_capacity(capacity, &self.target_file);
-        let mut reader = BufReader::with_capacity(capacity, &self.source_file);
+use super::FileCopy;
 
-        loop {
-            let buffer = reader.fill_buf().expect("error in the buffer");
-            let buffer_lenght = buffer.len();
-            if buffer_lenght == 0 {
-                break;
-            }
-            stream.write_all(buffer).expect("error to write");
-            reader.consume(buffer_lenght);
-            progress_bar.consume(buffer_lenght);
-        }
-    }
-}
 pub struct Copier {
     paused: bool,
 }
--- src/copier/mod.rs
+++ src/copier/mod.rs
@@ -1,4 +1,5 @@
 mod copier;
-pub use copier::FileCopy;
+pub mod file_copy;
 pub mod file_to_copy;
+pub use file_copy::FileCopy;
 pub use file_to_copy::FileToCopy;
