491ad5fb9cd077309232ed82c1fa91a7fd040c10
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-02 12:22:04 -0400

    🦄 refactor: use clap with Config

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,8 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+#[derive(Parser)]
 pub struct Config {
     pub source_paths: Vec<String>,
-    pub target_path: String,
-}
-impl Config {
-    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
-        let mut rev_args = args.collect::<Vec<String>>().into_iter().rev();
-        let target_path = match rev_args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a target_path string"),
-        };
-        let source_paths: Vec<String> = rev_args.collect();
-
-        Ok(Self {
-            source_paths,
-            target_path,
-        })
-    }
+    pub target_path: PathBuf,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,19 @@ mod copier_pool;
 mod progress_counter;
 #[cfg(test)]
 mod test;
-use std::{env, path::Path, process, time::Instant};
+use std::time::Instant;
+
+use clap::Parser;
 
 use crate::copier::FileToCopy;
-// TODO: apply this doc https://www.rust-lang.org/what/cli
+// TODO: apply this doc https://rust-cli.github.io/book/index.html
 fn main() {
-    let config = config::Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let config = config::Config::parse();
     let start = Instant::now();
     for source_path in config.source_paths {
         let mut folder = FileToCopy::from_path(&source_path);
         folder.load_files_from_path();
-        let target_path = Path::new(&config.target_path);
-        folder.copy_to(target_path);
+        folder.copy_to(config.target_path.as_path());
     }
     let duration = start.elapsed();
     println!();
