e7c8e55ce541803928240b325c3ce452d37dce4d
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-06 16:40:17 -0400

    ✨ feat: add more info to package

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@ mod config;
 pub mod copier;
 mod copier_pool;
 mod progress_counter;
-use std::time::Instant;
-
-use clap::Parser;
-
 use crate::copier::FileToCopy;
+use clap::Parser;
+use std::time::Instant;
+// TODO: add integration tests
 // TODO: apply this doc https://rust-cli.github.io/book/tutorial/testing.html
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::Config::parse();
     let start = Instant::now();
-    print!("{}", config.disable_logging);
+
     if let Some((target_path, source_paths)) = config.paths.split_last() {
         for source_path in source_paths {
             let mut folder = FileToCopy::from_path(source_path);
@@ -20,6 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     let duration = start.elapsed();
+
     println!();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
     Ok(())
